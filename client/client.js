ff2547b469a066642e457d245a78c1686b7048da
Wutsn <novak.martin.645@protonmail.ch>
2020-11-05 18:07:59 +0100

    update

--- client/client.js
+++ client/client.js
@@ -1,24 +1,23 @@
 class klientt {
     constructor(jmeno, host) {
-        this.jmeno = jmeno;
-        this.host = host;
-        this.clid = this.GRS(5);
-        this.memory = [];
-        this.uepause = true;
+        this.jmeno = jmeno; //name
+        this.host = host; //host ip
+        this.clid = this.GRS(5); //client id (if multile clients very useful)
+        this.memory = []; //memory block
     }
     socket_create() {
         this.socket = new WebSocket(this.host);
         this.socket.onopen = this.data_out("Connection was established...");
     }
-    read_data(){
+    read_data(){ //read from socket
         var superbridge = this;
         this.socket.onmessage = function(msg) {
             var brmsg = msg.data;
-            if(brmsg.includes("MSG-OK<->")) {
+            if(brmsg.includes("MSG-OK<->")) { //if ok, just forget msg
                 var msgkey = superbridge.arr_find(superbridge.memory ,brmsg.substring(10,13));
                 superbridge.memory.splice(msgkey[0],1);
                 console.log(superbridge.memory);
-            } else if(brmsg.includes("MSG-ERR<->")) {
+            } else if(brmsg.includes("MSG-ERR<->")) { //if err, resend 3 times (msgid is ok, but part of msg got currupted over the network or sth)
                 var msgkey = superbridge.arr_find(superbridge.memory ,brmsg.substring(11,14));
                 if(superbridge.memory[msgkey[0]][1] > 2) {
                     alert("Part of data frame dropped -> exiting. (3 attempts)");
@@ -29,10 +28,10 @@ class klientt {
                     superbridge.fail_toast(superbridge.memory[msgkey[0]][2]);
                     superbridge.memory[msgkey[0]][4] = 1;
                 }
-            } else if(brmsg.includes("UNKNOWN-ERR<->")) {
+            } else if(brmsg.includes("UNKNOWN-ERR<->")) { //msg completely corrupted or server is not able to read it
                 for(let x = 0; x < superbridge.memory.length; x++) {
-                    if(superbridge.memory[x][4] == 0) {
-                        if(superbridge.memory[x][3] == false) {
+                    if(superbridge.memory[x][4] == 0) { //if UNKNOWN-ERR do this
+                        if(superbridge.memory[x][3] == false) {// if not marked, resend
                             superbridge.fail_toast(superbridge.memory[x][2]);
                             superbridge.memory[x][3] = true;
                             superbridge.memory[x][1]++;
@@ -43,21 +42,21 @@ class klientt {
                                 }
                                 superbridge.memory[x][1]++;
                             } else {
-                                alert("Corrupted data. exiting. (5 attempts)");
+                                alert("Corrupted data. exiting. (5 attempts)"); //drop if resend not succeed in 5 tries
                                 superbridge.terminator(true);
                             }
                         }
                     }
                 }
             } else {
-                log(brmsg);
-                console.log(brmsg);
+                log(brmsg); //output data
+                console.log(brmsg); //same but to console
             }
         };
     }
-    toast(msg) {
+    toast(msg) { //push msg to memory and than loop tru memory and send
         var datalenght = msg.length;
-        if(datalenght <= 1900) {
+        if(datalenght <= 1900) { //max lenght 1900 data (rest, like name, id, len, brackets don't)
             var dmsg = [this.jmeno, this.clid, msg];
             var msgid = this.GRS(3);
             var finmsg = this.msg_pack(dmsg, msgid);
@@ -69,26 +68,26 @@ class klientt {
             alert("data too long ("+datalenght+" | max: 1900) <-> fix needed!");
         }
     }
-    fail_toast(msg) {
+    fail_toast(msg) { //send msg no matter what
         this.socket.send(msg);
     }
-    data_out(data) {
+    data_out(data) { //just log
         console.log(data);
     }
-    terminator(sec) {
+    terminator(sec) { //end connection
         if(sec == true) {
             this.socket.close();
             this.socket = null;
         }
     }
-    msg_pack(msg, id) {
+    msg_pack(msg, id) { //make msg compact, parse id with message (if msg fail, script can resend data)
         var text = ["0000", id, msg];
         text = JSON.stringify(text);
         var textlen = text.length;
         var textdata = text.replace(text.substring(2,6),("000" + textlen).slice(-4));
         return textdata;
     }
-    arr_find(haystack, needle) {  //max 2 dimensions
+    arr_find(haystack, needle) {  //find needle (value), max 2 dimensions
         var haystacklen = haystack.length;
         for(let x = 0; x < haystacklen-1; x++) {
             if(Array.isArray(haystack[x])){
@@ -106,7 +105,7 @@ class klientt {
         }
         return false;
     }
-    GRS(length) {
+    GRS(length) { //renerate random string 
         var result = '';
         var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         var charactersLength = characters.length;
@@ -116,23 +115,19 @@ class klientt {
         return result;
     }
 }
-
-var host = "ws://109.164.113.230:10000";
+var host = "ws://109.164.113.230:10000"; //your own ip w/port
 function start() {
-    name = prompt("Jaké je vaše jméno?");
-    klient = new klientt(name, host);
-    klient.socket_create();
-    klient.read_data();
+    name = prompt("Jaké je vaše jméno?"); //init for name
+    klient = new klientt(name, host); //init klientt 
+    klient.socket_create(); //connect
+    klient.read_data(); //prepare for reading
 }
-function stop() {
+function stop() { //terminate connection
     klient.terminator(true);
 }
-function write(msgg) {
+function write(msgg) { //handle function for input
     klient.toast(msgg);
 }
-// Utilities
-
-
 function log(msg){ document.getElementById("log").innerHTML+=msg+"<br>"; }
 function onkey(event){ if(event.keyCode==13){ write(document.getElementById("msg").value); } }
 function txt_write() {write(document.getElementById("msg").value);}
